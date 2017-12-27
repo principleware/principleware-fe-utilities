8a7650266a3f328ca824dc0a3f94f1ef9b6edb7a
Xiaolong Tang <xxlongtang@gmail.com>
2017-12-27 15:27:44 -0500

    k

---
+++ src/demo/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { replace } from 'principleware-fe-utilities';
+
+@Component({
+    selector: 'demo-app',
+    templateUrl: './app.component.html'
+})
+export class AppComponent {
+
+    constructor() {
+    }
+}
