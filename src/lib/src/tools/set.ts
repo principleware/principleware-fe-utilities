658222edee2321e2d4d49a21024ab6c23d9d4b70
Xiaolong Tang <xxlongtang@gmail.com>
2018-07-06 05:43:53 -0400

    Fix bugs: set

--- src/lib/src/tools/set.ts
+++ src/lib/src/tools/set.ts
@@ -3,10 +3,9 @@ export function diff<T, U>(firstSet: Array<T>,
     secondSet: Array<U>,
     predicate: (firstElem: T, secondElem: U) => boolean): Array<T> {
     return firstSet.filter((x: T) => {
-        const secondIndex = secondSet.findIndex((y: U) => {
+        return secondSet.some((y: U) => {
             return predicate(x, y);
         });
-        return secondIndex === -1;
     });
 }
 
