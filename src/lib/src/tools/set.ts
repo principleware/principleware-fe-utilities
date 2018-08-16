ba699c5743cbac33114c5bca1c8ae53d62b3987a
Xiaolong Tang <xxlongtang@gmail.com>
2018-08-15 21:51:33 -0400

    New feature: intersection of set

--- src/lib/src/tools/set.ts
+++ src/lib/src/tools/set.ts
@@ -9,3 +9,13 @@ export function diff<T, U>(firstSet: Array<T>,
     });
 }
 
+// firstSet intersection secondSet
+export function intersection<T, U>(firstSet: Array<T>,
+    secondSet: Array<U>,
+    predicate: (firstElem: T, secondElem: U) => boolean): Array<T> {
+    return firstSet.filter((x: T) => {
+        return secondSet.some((y: U) => {
+            return predicate(x, y);
+        });
+    });
+}
