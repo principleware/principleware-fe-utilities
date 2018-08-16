628605d5eafa1ee4ca5f253ee8102c258df4409b
Xiaolong Tang <xxlongtang@gmail.com>
2018-08-15 21:54:26 -0400

    Publish new version in dist

--- dist/src/lib/src/tools/set.d.ts
+++ dist/src/lib/src/tools/set.d.ts
@@ -1,1 +1,2 @@
 export declare function diff<T, U>(firstSet: Array<T>, secondSet: Array<U>, predicate: (firstElem: T, secondElem: U) => boolean): Array<T>;
+export declare function intersection<T, U>(firstSet: Array<T>, secondSet: Array<U>, predicate: (firstElem: T, secondElem: U) => boolean): Array<T>;
