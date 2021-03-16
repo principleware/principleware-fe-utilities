f4b4fa41883f6967b0251d514c5d17e26a279d58
Tom Tang <tom@fake.com>
2021-03-16 12:55:28 -0400

    Update gulp file for bump version

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,15 @@
 const { src, dest, parallel } = require('gulp');
 const typedoc = require("gulp-typedoc");
+const bumpversion = require('gulp-bump');
+
+// `fs` is used instead of require to prevent caching in watch (require caches)
+const fs = require('fs');
+
+function getVersion() {
+    return fs.readFileSync('./VERSION', 'utf8', function(err, data) {
+        return data;
+    });
+};
 
 function doc() {
     return src(["src/**/*.ts"])
@@ -25,5 +35,20 @@ function doc() {
         }));
 }
 
+function bump() {
+
+    const newVer = getVersion().trim();
+
+// bump versions on package/bower/manifest
+    return src(['./package.json', './projects/polpware/fe-data/package.json'])
+        .pipe(bumpversion({
+            version: newVer
+        }))
+        .pipe(dest(function(x) {
+            return x.base;
+        }));
+}
+
+exports.bump = bump;
 exports.doc = doc;
 exports.default = doc;
